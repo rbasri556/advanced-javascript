5dea4f78d6ca6957b31b0d25effd9931bd779170
Rabea Basri <rbasri556@gmail.com>
2021-02-03 09:19:11 -0800

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+let name = 12;
+
+if(name || name == 0){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+// false value
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// true value
+// '0'
+// " "
+// []
